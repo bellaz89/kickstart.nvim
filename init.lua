53d760b4dd6f1cb46dc055ff9dfd348f83b25f4e
Andrea Bellandi <andrea.bellandi@sckcen.be>
2025-01-24 15:44:47 +0100

    Add LintInfo command

--- init.lua
+++ init.lua
@@ -1027,5 +1027,15 @@ require('lazy').setup({
   },
 })
 
+vim.api.nvim_create_user_command('LintInfo', function()
+  local filetype = vim.bo.filetype
+  local linters = require('lint').linters_by_ft[filetype]
+
+  if linters then
+    print('Linters for ' .. filetype .. ': ' .. table.concat(linters, ', '))
+  else
+    print('No linters configured for filetype: ' .. filetype)
+  end
+end, {})
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
