ef63605027fd28fbd58de5f1b62246c1d6d2d719
Andrea Bellandi <andrea.bellandi@sckcen.be>
2025-04-02 11:15:32 +0200

    Added formatters and linters

--- init.lua
+++ init.lua
@@ -706,7 +706,9 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+
+        -- local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = {}
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
@@ -719,17 +721,20 @@ require('lazy').setup({
         }
       end,
       formatters_by_ft = {
+        markdown = { 'markdownlint' },
+        vhdl = { 'vsg' },
+        cpp = { 'clang-format' },
+        c = { 'clang-format' },
         lua = { 'stylua' },
         python = { 'isort', 'black' },
-        yaml = { 'yamlfix' },
-        vhdl = { 'vsg' },
-        xml = { 'xmlformatter' },
-        json = { 'fixjson' },
         bash = { 'beautysh' },
         sh = { 'beautysh' },
-        systemverilog = { 'verible' },
-        markdown = { 'markdown-cli2' },
+        json = { 'fixjson' },
         rst = { 'rstfmt' },
+        yaml = { 'yamlfix' },
+        xml = { 'xmlformatter' },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
