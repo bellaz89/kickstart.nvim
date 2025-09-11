45dc3d8e65b62c2bd871792c29e5b2b090bff504
Andrea Bellandi <andrea.bellandi@sckcen.be>
2025-09-11 08:56:37 +0200

    chore: removed commented nvim-lint lines

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -8,8 +8,6 @@ return {
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
         vhdl = { 'vsg' },
-        -- cpp = { 'cppcheck', 'clangtidy' },
-        -- c = { 'cppcheck', 'clangtidy' },
         lua = { 'luacheck' },
         python = { 'pylint' },
         bash = { 'bash' },
