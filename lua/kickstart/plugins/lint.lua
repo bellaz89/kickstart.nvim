8eef11af5e79afb35f17a1c18f0a002ea8fb1b2f
Andrea Bellandi <andrea.bellandi@sckcen.be>
2025-09-11 08:59:24 +0200

    fix: removed cppcheck in nvim-lint. The linting should be done by clangd

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -9,7 +9,6 @@ return {
         markdown = { 'markdownlint' },
         vhdl = { 'vsg' },
         lua = { 'luacheck' },
-        python = { 'pylint' },
         bash = { 'bash' },
         json = { 'jsonlint' },
         rst = { 'vale' },
@@ -19,74 +18,6 @@ return {
         tcl = { 'nagelfar' },
         verilog = { 'verilator' },
       }
-
-      local cppcheck = lint.linters.cppcheck
-      cppcheck.args = {
-        '--enable=all',
-        '--template=gcc',
-        '--suppress=unusedFunction',
-        '--suppress=missingIncludeSystem',
-        '--suppress=unmatchedSuppression:*',
-        '--suppress=*:*spdlog\\*',
-        '--suppress=*:*catch2\\*',
-        '--suppress=*:*doctest\\*',
-        '--suppress=*:*trompeloeil\\*',
-        function()
-          if vim.bo.filetype == 'cpp' then
-            return '--language=c++'
-          else
-            return '--language=c'
-          end
-        end,
-        '--inline-suppr',
-        '--verbose',
-        '--quiet',
-        function()
-          if vim.fn.isdirectory 'build' == 1 then
-            return '--cppcheck-build-dir=build'
-          else
-            return nil
-          end
-        end,
-        '--template={file}:{line}:{column}: [{id}] {severity}: {message}',
-      }
-
-      --local clangtidy = lint.linters.clangtidy
-      --clangtidy.args = {
-      --  '--quiet',
-      --}
-
-      -- To allow other plugins to add linters to require('lint').linters_by_ft,
-      -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      --
-      -- However, note that this will enable a set of default linters,
-      -- which will cause errors unless these tools are available:
-      -- {
-      --   clojure = { "clj-kondo" },
-      --   dockerfile = { "hadolint" },
-      --   inko = { "inko" },
-      --   janet = { "janet" },
-      --   json = { "jsonlint" },
-      --   markdown = { "vale" },
-      --   rst = { "vale" },
-      --   ruby = { "ruby" },
-      --   terraform = { "tflint" },
-      --   text = { "vale" }
-      -- }
-      --
-      -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
