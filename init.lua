ff8822734972f770990ee42110c3c15191e463e5
Andrea Bellandi <andrea.bellandi@sckcen.be>
2025-09-29 12:29:11 +0200

    feat: add pylsp-mypy

--- init.lua
+++ init.lua
@@ -556,6 +556,17 @@ require('lazy').setup({
         end,
       }
 
+      -- enable mypy by default
+      require('lspconfig').pylsp.setup {
+        settings = {
+          pylsp = {
+            plugins = {
+              mypy = { enabled = true, live_mode = false },
+            },
+          },
+        },
+      }
+
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
